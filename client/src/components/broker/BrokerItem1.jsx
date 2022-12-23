883d11ca8179a13f71ee8be6af457ca2330867df
hiten <hitenkhatri14@gmail.com>
2022-12-23 23:18:50 +0530

    changes

--- client/src/components/broker/BrokerItem1.jsx
+++ client/src/components/broker/BrokerItem1.jsx
@@ -6,13 +6,12 @@ const BrokerItem1 = () => {
             <div className='broker-home'>
                 <div className="broker-home1 flex">
                     <div className="broker-home11">
-                        <div className="broker-home111">
-                            <div className="broker-home-btn1">
-                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <div className="broker-home111 mb-8">
+                            <div className="broker-home-btn0 flex items-center">
+                                <div className="broker-add">
                                     +
-                                    <span class="sr-only">add</span>
-                                </button>
-                                <p className="text-blue-600">Add New Client</p>
+                                </div>
+                                <p>Add New Client</p>
                             </div>
                         </div>
                         <div className="broker-home111 mb-7">
@@ -23,39 +22,39 @@ const BrokerItem1 = () => {
                         </div>
                         <div className="broker-home111 mb-7">
                             <div className="flex">
-                                <img src="/static/images/Path 11488.png" alt="" />
+                                <img src="/static/images/Path11488.png" alt="" />
                                 <p className='ml-4'>Properties</p>
                             </div>
                         </div>
                         <div className="broker-home111 mb-7">
                             <div className="flex">
-                                <img src="/static/images/Group 19843.png" alt="" />
+                                <img src="/static/images/Group19843.png" alt="" />
                                 <p className='ml-4'>Customer Management</p>
                             </div>
                         </div>
                         <div className="broker-home111 mb-7">
                             <div className="flex">
-                                <img src="/static/images/Path 11515.png" alt="" />
+                                <img src="/static/images/Path11515.png" alt="" />
                                 <p className='ml-4'>Visit Management</p>
                             </div>
                         </div>
                         <div className="broker-home111 mb-7">
                             <div className="flex">
-                                <img src="/static/images/Path 11546.png" alt="" />
+                                <img src="/static/images/Path11546.png" alt="" />
                                 <p className='ml-4'>Payment History</p>
                             </div>
                         </div>
                         <div className="broker-home111 mb-7">
                             <div className="flex">
-                                <img src="/static/images/Group 19872.png" alt="" />
+                                <img src="/static/images/Group19872.png" alt="" />
                                 <p className='ml-4'>Raise request for Loan support</p>
                             </div>
                         </div>
                     </div>
-                    <div className="broker-home12">
+                    <div className="broker-home12 flex">
                         <div className="broker-home121">
-                            <div className="broker-home1211 flex justify-between">
-                                <div className="broker-home12111">
+                            <div className="broker-home1211">
+                                <div className="broker-home12111 flex justify-between">
                                     <b>Dashboard</b>
                                     <div>
                                         !
@@ -64,72 +63,74 @@ const BrokerItem1 = () => {
                                 <div className="broker-home12112 flex">
                                     <div className="broker-home12a">
                                         <p>Total Sales</p>
-                                        <p className="text-xl">120</p>
+                                        <p className="text-xl font-semibold">120</p>
                                     </div>
-                                    <div className="broker-home12a">
+                                    <div className="broker-home12a broker-home12-border">
                                         <p>Total Earning</p>
-                                        <p className="text-xl">10Lac</p>
+                                        <p className="text-xl font-semibold">10Lac</p>
                                     </div>
-                                    <div className="broker-home12a">
+                                    <div className="broker-home12a broker-home12-border">
                                         <p>Total Clients</p>
-                                        <p className="text-xl">22</p>
+                                        <p className="text-xl font-semibold">22</p>
                                     </div>
-                                    <div className="broker-home12a">
+                                    <div className="broker-home12a broker-home12-border">
                                         <p>Follow Up</p>
-                                        <p className="text-xl">50</p>
+                                        <p className="text-xl font-semibold">50</p>
                                     </div>
                                     <div className="broker-home12a">
                                         <p>Total Meetings</p>
-                                        <p className="text-xl">110</p>
+                                        <p className="text-xl font-semibold">110</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="broker-home1212">
-                                <div className="broker-home12121 flex justify-between">
+                                <div className="broker-home12121 items-center flex justify-between">
                                     <div>
                                         <p>Clients</p>
                                     </div>
                                     <div className='flex'>
-                                        <button className="broker-home-btn broker-home-btn1 mr-4">
+                                        <button className="broker-home-btn broker-home-btn1 mr-4 flex items-center">
                                             <p>Today</p>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="ml-2 bi bi-chevron-down" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
                                             </svg>
                                         </button>
-                                        <button className="broker-home-btn broker-home-btn1">
+                                        <button className="broker-home-btn broker-home-btn2 flex items-center">
                                             <p>Lorem, ipsum</p>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="ml-2 bi bi-chevron-down" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
                                             </svg>
                                         </button>
                                     </div>
                                 </div>
                                 <div className="broker-home12122">
-                                    <div className="broker-home-table1 flex">
-                                        <img className='ml-3' src="/static/images/Rectangle 2151.png" alt="" />
-                                        <p>Sagar Sharma</p>
-                                    </div>
-                                    <div className="broker-home-table1 flex">
-                                        <img className='ml-3' src="/static/images/Path 11489.png" alt="" />
-                                        <p>Uttam Nagar (Delhi)</p>
-                                    </div>
-                                    <div className="broker-home-table1 flex">
-                                        <img className='ml-3' src="/static/images/Path 11490.png" alt="" />
-                                        <p>4(BHK) at Gaur’s City</p>
-                                    </div>
-                                    <div className="broker-home-table1 flex">
-                                        <img className='ml-3' src="/static/images/Path 11488 (1).png" alt="" />
-                                        <p>70-01 Lack-one Crore</p>
-                                    </div>
-                                    <div className="broker-home-table1 flex">
-                                        <div className="broker-home-pill ml-4">
-                                            <img src="/static/images/Vector.png" alt="" />
+                                    <div className="broker-home-table flex">
+                                        <div className="broker-home-table1 broker-home-table11 flex items-center">
+                                            <img className='mr-1.5' src="/static/images/Rectangle2151.png" alt="" />
+                                            <p>Sagar Sharma</p>
                                         </div>
-                                        <div className="broker-home-pill ml-4">
-                                            <img src="/static/images/Vector (1).png" alt="" />
+                                        <div className="broker-home-table1 broker-home-table11 flex items-center">
+                                            <img className='mr-1.5' src="/static/images/Path11489.png" alt="" />
+                                            <p>Uttam Nagar (Delhi)</p>
                                         </div>
-                                        <div className="broker-home-pill ml-4">
-                                            <img src="/static/images/Group.png" alt="" />
+                                        <div className="broker-home-table1 broker-home-table11 flex items-center">
+                                            <img className='mr-1.5' src="/static/images/Path11490.png" alt="" />
+                                            <p>4(BHK) at Gaur’s City</p>
+                                        </div>
+                                        <div className="broker-home-table1 broker-home-table11 flex items-center">
+                                            <img className='mr-1.5' src="/static/images/Path11488(1).png" alt="" />
+                                            <p>70-01 Lack-one Crore</p>
+                                        </div>
+                                        <div className="broker-home-table1 flex items-center">
+                                            <div className="broker-home-pill ml-4">
+                                                <img src="/static/images/Vector.png" alt="" />
+                                            </div>
+                                            <div className="broker-home-pill ml-4">
+                                                <img src="/static/images/Vector(1).png" alt="" />
+                                            </div>
+                                            <div className="broker-home-pill ml-4">
+                                                <img src="/static/images/Group.png" alt="" />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
