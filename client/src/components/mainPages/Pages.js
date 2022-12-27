97baff2d49573e45aa72bfe63efb511887160503
hiten <hitenkhatri14@gmail.com>
2022-12-27 23:07:37 +0530

    changes

--- client/src/components/mainPages/Pages.js
+++ client/src/components/mainPages/Pages.js
@@ -8,6 +8,7 @@ import BrokerProfile from "../broker/BrokerProfile";
 import { GlobalState } from "../../GlobalState";
 import AdminDashBoard from "../Admin/AdminDashBoard";
 import BrokerManage from "../Admin/BrokerManage";
+import CustomerManagement from "../broker/CustomerManagement";
 
 
 const Pages = ({setPop,resetPop,setResetPop,stepPop,setStepPop}) => {
@@ -26,6 +27,7 @@ const Pages = ({setPop,resetPop,setResetPop,stepPop,setStepPop}) => {
         />
         <Route exact path="/login" element={<Login resetPop={resetPop} setResetPop={setResetPop} stepPop={stepPop} setStepPop={setStepPop} />} />
         <Route exact path="/brokerProfile" element={<BrokerProfile/>}/>
+        <Route exact path="/brokerProfile/customerManagement" element={<CustomerManagement/>}/>
 
         <Route exact path="/dashboard" element={isAdmin ? <AdminDashBoard/> : <Notfound/>}/>
         <Route exact path="/dashboard/manageBrok" element={ isAdmin ? <BrokerManage/> : <Notfound/>}/>
