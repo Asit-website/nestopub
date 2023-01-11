3a1b5d6e60b50e0457fb390ba82520a5084cc7dd
hiten <hitenkhatri14@gmail.com>
2023-01-12 02:28:00 +0530

    changes

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -1,98 +1,69 @@
 const mongoose = require("mongoose");
 const propertySchema = new mongoose.Schema({
-  property_id:{
-    type:String,
-    unique:true,
-    trim:true,
-    required:false,
-    set: b => b === '' ? undefined : b
+  propertyContent: {
+    type: String,
   },
-  propertyContent:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  propertyPrice: {
+    type: Number,
   },
-  propertyPrice:{
-    type:Number,
-    required:false,
-    set: b => b === '' ? undefined : b
+  category: {
+    type: String,
   },
-  category:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  location: {
+    type: String,
   },
-  location:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  propertyArea: {
+    type: String,
   },
-  propertyArea:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  propertyDescription: {
+    type: String,
   },
-  propertyDescription:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  parking: {
+    type: Number,
   },
-  parking:{
-    type:Number,
-    required:false,
-    set: b => b === '' ? undefined : b
+  venue: {
+    type: String,
+    default:"abc",
   },
-  venue:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  propertyOwners: {
+    type: String,
+    default:"abc",
   },
-  propertyOwners:{
-    type:String,
-    required:false,
-    set: b => b === '' ? undefined : b
+  ownerImages: {
+    type: Object,
+    default:{},
   },
-  ownerImages:{
-    type:Object,
-      required: false,
-    set: xy => xy === '' ? undefined : xy
+  images: {
+    type: Array,
   },
-  images:{
-    type:Array,
-    required:false,
-    set: b => b === '' ? undefined : b
+  bedroom: {
+    type: Number,
   },
-  bedroom:{
-    type:Number,
-    required:false,
-    set: b => b === '' ? undefined : b
+  Guest: {
+    type: Number,
   },
-  Guest:{
-    type:Number,
-    required:false,
-    set: b => b === '' ? undefined : b
+  bathRoom: {
+    type: Number,
   },
-  bathRoom:{
-    type:Number,
-    required:false,
-    set: b => b === '' ? undefined : b
+  sold: {
+    type: Number,
+    default: 0
   },
-  sold:{
-    type:Number,
-    default:0
+  checks: {
+    type: Boolean,
+    default: false
   },
-  checks:{
-    type:Boolean,
-    default:false
+  likes: {
+    type: Array,
+    default: []
   },
-  likes:[{type:mongoose.Types.ObjectId, ref:'user'}],
-  user:{
-    type:mongoose.Types.ObjectId,
-    ref:'user'
+  user: {
+    type: String,
+    default: "user1"
   }
-},{
-    timestamps:true
+}, {
+  timestamps: true
 });
 
 
-module.exports = mongoose.model("property",propertySchema);
+module.exports = mongoose.model("property", propertySchema);
--- server.js
+++ server.js
@@ -4,42 +4,49 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
-const socketServerRouter=require('./Router/socketServer');
-const http=require('http');
+const socketServerRouter = require('./Router/socketServer');
+const http = require('http');
 const app = express();
+const bodyParser=require('body-parser');
 
 const server = http.createServer(app);
 
 //middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-app.use(fileUpload({
-    useTempFiles:true
-}))
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static("uploads"));
+
+// app.use(fileUpload({
+//     useTempFiles: true
+// }))
 
 app.use('/socketServer', socketServerRouter({
     server
 }));
 
-app.use('/api',require('./Router/authRouter'));
+app.use('/api', require('./Router/authRouter'));
 app.use('/api', require('./Router/brokerRouter'));
-app.use('/api',require('./Router/upload'));
+app.use('/api', require('./Router/upload'));
 app.use('/api', require('./Router/clientRouter'));
-app.use('/user',require('./Router/ContactRouter'));
-app.use('/api',require('./Router/propertyRouter'));
-app.use('/api',require('./Router/categoryRouter'));
-app.use('/chat',require('./Router/chatRouter'));
+app.use('/user', require('./Router/ContactRouter'));
+app.use('/api', require('./Router/propertyRouter'));
+app.use('/api', require('./Router/categoryRouter'));
+app.use('/chat', require('./Router/chatRouter'));
 
 
 const URI = process.env.MONGODB_URL;
-mongoose.connect(URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
     console.log("connection successfull");
-}).catch(()=>{
+}).catch(() => {
     console.log("connection is not successfull");
 })
 
@@ -50,6 +57,6 @@ const PORT = process.env.PORT || 5000;
 //     console.log('server is runing on port',PORT);
 // });
 
-server.listen(PORT,()=>{
-    console.log('server is runing on port',PORT);
+server.listen(PORT, () => {
+    console.log('server is runing on port', PORT);
 });
